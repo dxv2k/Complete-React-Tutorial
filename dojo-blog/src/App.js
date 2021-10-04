6de7eb9e426161d4218f0ad26002f07b7ec82d6f
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-10-04 10:30:42 +0700

    done lesson 12

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,19 @@
+// import './App.css'; # don't need it now 
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = "Welcome to the new blog"; 
+  const likes = 50; 
+  const link = "www.uit.edu.vn"; 
+  return (
+    <div className="App">
+      <Navbar/> 
+      <div className="content">
+        <Home/> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
